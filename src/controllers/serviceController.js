4fcc4dcca486bb96613aff6ef912ccdd24185291
patricklohn <patricklohn3@gmail.com>
2025-02-17 19:28:34 -0300

    Criado Crud para o Projeto Create, Read, Update, Delete.

--- src/controllers/serviceController.js
+++ src/controllers/serviceController.js
@@ -34,9 +34,74 @@ async function getAllService(req,res){
     }
 }
 
+async function getService(req,res){
+    try {
+        const id = req.params.id;
+        const service = await ServiceModel.findById(id)
+
+        if(!service){
+            res.status(404).json({menssage: "Serviço não encontrado."})
+            return
+        }
+
+        res.status(200).json(service);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Internal error Server!"});
+    }
+}
+
+async function deleteService(req,res){
+    try {
+        const id = req.params.id;
+        const service = await ServiceModel.findById(id)
+
+        if(!service){
+            res.status(404).json({menssage: "Serviço não encontrado."})
+            return
+        }
+
+        const serviceDelete = await ServiceModel.findByIdAndDelete(id);
+        res.status(200).json(serviceDelete);
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: "Internal error Server!"});
+    }
+}
+
+async function editService(req,res){
+    try {
+        const id = req.params.id; 
+        const service = {
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+            image: req.body.image,
+        };
+
+        if(!service){
+            res.status(404).json({menssage: "Serviço não encontrado."})
+            return
+        }
+
+        const serviceUpdate = await ServiceModel.findByIdAndUpdate(id, service); 
+        res.status(200).json({message:"Cadastro alterado com sucesso",serviceUpdate});
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: "Internal error Server!"});
+    }
+}
+
 //module.exports = serviceController;
 
 export default {
     createService,
-    getAllService
+    getAllService,
+    getService,
+    deleteService,
+    editService
 };
+
