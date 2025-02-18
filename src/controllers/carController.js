8838b61cca53f76133c7240e69b50daf31077f9f
patricklohn <patricklohn3@gmail.com>
2025-02-17 21:43:53 -0300

    criado função para edição e finalizado backend

--- src/controllers/carController.js
+++ src/controllers/carController.js
@@ -56,6 +56,11 @@ async function getCar(req,res){
         const id = req.params.id; 
         const carGet = await Car.findById(id);
 
+        if(!carGet){
+            res.status(404).json({message:"Não foi encontrado nenhum Carro!!"})
+            return 
+        }
+
         res.status(200).json(carGet);
 
     } catch (error) {
@@ -69,7 +74,41 @@ async function deleteCar(req,res){
         const id = req.params.id;
         const deleteCar = await Car.findByIdAndDelete(id);
 
+        if(!carGet){
+            res.status(404).json({message:"Não foi encontrado nenhum Carro!!"})
+            return
+        }
+
         res.status(200).json({message: "Car deletado com sucesso", deleteCar})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({menssage: "Internal Error Server!"})
+    }
+}
+
+async function updateCar(req,res){
+    try {
+        const id = req.params.id;
+        const car = {
+            name: req.body.name,
+            brand: req.body.brand,
+            description: req.body.description,
+            model: req.body.model,
+            budget: req.body.budget,
+            image: req.body.image,
+            services: req.body.services
+        }
+
+        // budget < valor dos serviços 
+
+        if(car.services && !checkCarBudget(car.budget, car.services)){
+            res.status(406).json({message:"buget é menor services price"})
+            return
+        }
+
+        const carUpdate = await Car.findByIdAndUpdate(id, car);
+        res.status(200).json({carUpdate, menssage: "Dados atualizados!"})
+        
     } catch (error) {
         console.log(error)
         res.status(500).json({menssage: "Internal Error Server!"})
@@ -80,5 +119,6 @@ export default {
     createCar,
     getAllCar,
     getCar,
-    deleteCar
+    deleteCar,
+    updateCar
 }
--- src/router/carRouter.js
+++ src/router/carRouter.js
@@ -7,5 +7,6 @@ carRouter.post("/carCreate", carController.createCar);
 carRouter.get("/carGet", carController.getAllCar);
 carRouter.get("/carGet/:id", carController.getCar);
 carRouter.delete("/carGet/:id", carController.deleteCar);
+carRouter.put("/carGet/:id", carController.updateCar);
 
 export default carRouter;
